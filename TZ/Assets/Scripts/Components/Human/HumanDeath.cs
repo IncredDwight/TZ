a1926349879bd778576d1f6faef1db56e2c952b3
VYO <yaravirus86@gmail.com>
2024-06-09 17:16:56 -0400

    Human death tweak

--- TZ/Assets/Scripts/Components/Human/HumanDeath.cs
+++ TZ/Assets/Scripts/Components/Human/HumanDeath.cs
@@ -3,18 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(HumanRoaming))]
+[RequireComponent(typeof(Collider))]
 public class HumanDeath : MonoBehaviour, IDie
 {
-    private HumanRoaming _humanRoaming;
-
     private Transform _target;
     private float _speed = 15.0f;
     private float _destroyDistance = 1f;
-
-    private void Awake()
-    {
-        _humanRoaming = GetComponent<HumanRoaming>();
-    }
 
     private void Update()
     {
@@ -28,7 +22,8 @@ public class HumanDeath : MonoBehaviour, IDie
 
     public void Die()
     {
-        _humanRoaming.enabled = false;
+        GetComponent<HumanRoaming>().enabled = false;
+        GetComponent<Collider>().enabled = false;
         GameEvents.SendOnHumanDeath();
 
         _target = FindObjectOfType<PlayerMovement>().transform;
